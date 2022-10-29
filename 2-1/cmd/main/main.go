56c323cbba4179f30e3bf7846a27f303b0514a2c
Aramassa <52207597+Aramassa@users.noreply.github.com>
2022-10-29 14:37:16 +0000

    pkg に Example struct を定義

--- 2-1/cmd/main/main.go
+++ 2-1/cmd/main/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"2-1/internal/example"
+	pkg "2-1/pkg/example"
 	"fmt"
 )
 
 func main() {
 	fmt.Println("Hello")
 
-	ex := example.Example{}
-	ex.MyName()
+	ex := example.Example{Name: "Yosuke Ex of intenrl", Age: 41}
+	fmt.Println(ex.MyName())
+
+	ex2 := pkg.Example{Name: "Yosuke Ex of pkg", Age: 41}
+	fmt.Println(ex2.MyName())
 }
