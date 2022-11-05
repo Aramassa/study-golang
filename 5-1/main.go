c1055c3fc275f81d121837ceac232f732bc103b9
Aramassa <52207597+Aramassa@users.noreply.github.com>
2022-11-05 07:09:34 +0000

    /test page を追加

--- 5-1/main.go
+++ 5-1/main.go
@@ -14,12 +14,20 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 
 	fmt.Fprintln(w, "Hello world!")
+}
 
+func TestHandler(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
+
+	fmt.Fprintln(w, "Hello test world!")
 }
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/test", TestHandler)
 
 	http.ListenAndServe(":8080", handlers.CompressHandler(r))
 }
